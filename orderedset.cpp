2113acbae4d269532e304db00f587e3bc064a3c0
Vitor Rossi Speranza <vrsperanza@gmail.com>
2018-09-18 12:27:08 -0300

    Ordered set

---
+++ orderedset.cpp
@@ -0,0 +1,8 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename tA, typename tB=null_type> using ord_set = tree<tA, tB, less<tA>, rb_tree_tag, tree_order_statistics_node_update>;
+// map: tA -> tB com comparador less<tA>
+// pode usar como um map normalmentez
+// s.find_by_order(k) :: retorna iterador para o k-esimo elemento (0-index) (ou s.end())
+// s.order_of_key(x)  :: retorna a qtd de elementos estritamente menores que x
