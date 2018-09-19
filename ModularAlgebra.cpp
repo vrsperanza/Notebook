e48763ab6d9b83e90dfe06c731732da7a6c2c006
Vitor Rossi Speranza <vrsperanza@gmail.com>
2018-09-19 19:30:41 -0300

    Modular algebra

---
+++ ModularAlgebra.cpp
@@ -0,0 +1,28 @@
+const int MOD = ?;
+
+inline int mul(int a, int b){
+	return int(a * 1LL * b % MOD);
+}
+
+inline int fpow(int a, int n){
+	int res = 1;
+	for(; n > 0; n >>= 1){
+		if(n & 1) res = mul(res, a);
+		a = mul(a, a);
+	}
+	
+	return res;
+}
+
+inline int inv(int a){
+	int ia = fpow(a, MOD - 2);
+	assert(mul(a, ia) == 1);
+	return ia;
+}
+
+inline int sum(int a, int b){
+	a += b;
+	while(a >= MOD) a -= MOD;
+	while(a < 0) a += MOD;
+    return a;
+}
