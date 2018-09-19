aaf85a8299767c7dcdfb8c10237f6ff9a653e9dc
Vitor Rossi Speranza <vrsperanza@gmail.com>
2018-09-19 19:32:52 -0300

    Template

---
+++ Template.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastcin ios_base::sync_with_stdio(false)
+#define precisecout cout << fixed << setprecision(10)
+#define pb push_back
+#define mk make_pair
+#define mkt make_tuple
+#define binary(x) bitset<8*sizeof(x)>(x)
+#define zero(x) memset(x, 0, sizeof(x))
+#define set(x, i) memset(x, i, sizeof(x))
+#define ii pair<int, int>
+#define iii tuple<int, int, int>
+#define ll long long
+#define inf 0x3f3f3f3f
+
+#ifdef ebug
+#define dprintf printf
+#else
+#define dprintf if(false) printf
+#endif
+#define DEBUG 0
+
+int main(){
+	
+}
