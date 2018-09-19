eb66d3b61902e1920162d9f683a60b6dd224cf15
Vitor Rossi Speranza <vrsperanza@gmail.com>
2018-09-19 19:34:38 -0300

    Update OrderedSet.cpp

--- OrderedSet.cpp
+++ OrderedSet.cpp
@@ -3,6 +3,6 @@
 using namespace __gnu_pbds;
 template <typename tA, typename tB=null_type> using ord_set = tree<tA, tB, less<tA>, rb_tree_tag, tree_order_statistics_node_update>;
 // map: tA -> tB com comparador less<tA>
-// pode usar como um map normalmentez
+// pode usar como um map normalmente
 // s.find_by_order(k) :: retorna iterador para o k-esimo elemento (0-index) (ou s.end())
 // s.order_of_key(x)  :: retorna a qtd de elementos estritamente menores que x
